0f6f2689dbd358e3f04a28657cf836c5e18d749a
Lusha Liang <lushaliang@Lushas-MacBook-Air.local>
2020-11-30 20:08:53 -0800

    new app edits

--- app.R
+++ app.R
@@ -1,33 +1,9 @@
 library(tidyverse)
 library(shiny)
 library(caret)
-
-# Read data 
-datacomplete = read_csv("./datacomplete.csv") %>%
-  mutate_at(c("admitted", "ethnicity_race", "asthma", "diabetes"), as.factor) %>%
-  select(admitted, age, bmi_value, systolic_bp_value, ethnicity_race, asthma, diabetes)
-
-# Build random forest model in caret
-myFolds = createFolds(datacomplete$admitted, k = 5)
-rfControl = trainControl(
-  summaryFunction = twoClassSummary,
-  classProbs = TRUE, 
-  verboseIter = TRUE,
-  savePredictions = TRUE,
-  index = myFolds
-)
-
-model = caret::train(
-  admitted ~., 
-  data = datacomplete,
-  metric = "ROC",
-  method = "ranger",
-  trControl = rfControl
-)
-
-####################################
-# UI                               #
-####################################
+library(ranger)
+library(e1071)
+library(data.table)
 
 ui = fluidPage(headerPanel('COVID-19 Pediatric Hospitalization Risk Predictor'),
                
@@ -72,11 +48,30 @@ ui = fluidPage(headerPanel('COVID-19 Pediatric Hospitalization Risk Predictor'),
                )
 )
 
-####################################
-# Server                           #
-####################################
+# Read data 
+datacomplete = read_csv("./datacomplete.csv") %>%
+  mutate_at(c("admitted", "ethnicity_race", "asthma", "diabetes"), as.factor) %>%
+  select(admitted, age, bmi_value, systolic_bp_value, ethnicity_race, asthma, diabetes)
 
-shinyServer(function(input, output, session) {
+# Build random forest model in caret
+myFolds = createFolds(datacomplete$admitted, k = 5)
+rfControl = trainControl(
+  summaryFunction = twoClassSummary,
+  classProbs = TRUE, 
+  verboseIter = TRUE,
+  savePredictions = TRUE,
+  index = myFolds
+)
+
+model = train(
+  admitted ~., 
+  data = datacomplete,
+  metric = "ROC",
+  method = "ranger",
+  trControl = rfControl
+)
+
+server = function(input, output) {
   
   # Input Data
   datasetInput = reactive({  
@@ -90,10 +85,13 @@ shinyServer(function(input, output, session) {
     admitted = "admitted"
     df = rbind(admitted, df)
     input = transpose(df)
-    write.table(input,"input.csv", sep=",", quote = FALSE, row.names = FALSE, col.names = FALSE)
+    write.table(input, "input.csv", sep=",", quote = FALSE, row.names = FALSE, col.names = FALSE)
     
     test = read_csv("input.csv") %>%
-      mutate(admitted = factor(admitted, levels = c("yes", "no")),
+      mutate(admitted = factor(admitted, levels = c("no", "yes")),
+             age = as.numeric(age),
+             bmi_value = as.numeric(bmi_value),
+             systolic_bp_value = as.numeric(systolic_bp_value),
              ethnicity_race = factor(ethnicity_race, levels = c("american indian", "asian", "caucasian", "black",
                                                                 "latino", "multiple")),
              asthma = factor(asthma, levels = c("0", "1")),
@@ -121,5 +119,7 @@ shinyServer(function(input, output, session) {
     } 
   })
   
-})
+}
 
+# Run the application 
+shinyApp(ui = ui, server = server)
