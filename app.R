8d699bbbc531f2910dfecdb29acbaa67700b9812
Lusha Liang <lushaliang@Lushas-MacBook-Air.local>
2020-11-30 19:02:14 -0800

    edits to app

--- app.R
+++ app.R
@@ -1,15 +1,34 @@
-# Import libraries
-library(data.table)
-library(randomForest)
+library(tidyverse)
 library(shiny)
+library(caret)
+library(data.table)
 
 # Read data 
-datacomplete = read.csv("./datacomplete.csv") 
+datacomplete = read_csv("./datacomplete.csv") %>%
+  mutate_at(c("admitted", "ethnicity_race", "asthma", "diabetes"), as.factor) %>%
+  select(admitted, age, bmi_value, systolic_bp_value, ethnicity_race, asthma, diabetes)
 
-# Build model
-model = randomForest(admitted ~ ., data = datacomplete, ntree = 500, mtry = 6, importance = TRUE)
+# Build random forest model in caret
+myFolds = createFolds(datacomplete$admitted, k = 5)
+rfControl = trainControl(
+  summaryFunction = twoClassSummary,
+  classProbs = TRUE, 
+  verboseIter = TRUE,
+  savePredictions = TRUE,
+  index = myFolds
+)
 
-# User interface                   
+model = caret::train(
+  admitted ~., 
+  data = datacomplete,
+  metric = "ROC",
+  method = "ranger",
+  trControl = rfControl
+)
+
+####################################
+# UI                               #
+####################################
 
 ui = fluidPage(headerPanel('COVID-19 Pediatric Hospitalization Risk Predictor'),
                
@@ -54,9 +73,11 @@ ui = fluidPage(headerPanel('COVID-19 Pediatric Hospitalization Risk Predictor'),
                )
 )
 
-# Server
+####################################
+# Server                           #
+####################################
 
-shinyServer(function(input, output, session) {
+server = function(input, output, session) {
   
   # Input Data
   datasetInput = reactive({  
@@ -70,7 +91,7 @@ shinyServer(function(input, output, session) {
     admitted = "admitted"
     df = rbind(admitted, df)
     input = transpose(df)
-    write.table(input, "input.csv", sep=",", quote = FALSE, row.names = FALSE, col.names = FALSE)
+    write.table(input,"input.csv", sep=",", quote = FALSE, row.names = FALSE, col.names = FALSE)
     
     test = read_csv("input.csv") %>%
       mutate(admitted = factor(admitted, levels = c("yes", "no")),
@@ -80,7 +101,7 @@ shinyServer(function(input, output, session) {
              diabetes = factor(diabetes, levels = c("0", "1"))
       )
     
-    Output = predict(model, test, type ="prob")
+    Output = predict(model, test, type="prob")
     print(Output)
     
   })
@@ -101,4 +122,9 @@ shinyServer(function(input, output, session) {
     } 
   })
   
-})
+}
+
+####################################
+# Create the shiny app             #
+####################################
+shinyApp(ui = ui, server = server)
